b7d5387797e5f08fe251a70e071c9b9e18b9be3e
CarolyneNjoki72 <161461568+CarolyneNjoki72@users.noreply.github.com>
2025-07-03 12:56:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(
+    page_title='Kenya's Debt Analysis',
+    page_icon='💰','<img src="https://upload.wikimedia.org/wikipedia/commons/4/49/Flag_of_Kenya.svg" width="60">',
+    layout='wide',
+    initial_sidebar_state='expanded')
+
+alt.themes.enable('dark')
+
+#df1 = pd.read_csv('kenya_debt_analysis.csv')
+
+#with st.sidebar:
+
+
